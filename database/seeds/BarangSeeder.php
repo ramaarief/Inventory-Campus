70007fc91bc7a9a0eb26db77359c8ba2b658db74
ramaarief <ramaarief45@gmail.com>
2020-04-29 20:16:54 +0700

    Update BarangSeeder.php

--- database/seeds/BarangSeeder.php
+++ database/seeds/BarangSeeder.php
@@ -13,11 +13,11 @@ class BarangSeeder extends Seeder
     public function run()
     {
         $listBarang = [
-            ['ruangan_id' => '1',  'name' => 'Meja',         'total' => '25', 'broken' => '3', 'photo' => 'foto1.jpg',  'created_by' => '1', 'updated_by' => '1'],
-            ['ruangan_id' => '3',  'name' => 'Papan_Tulis',  'total' => '2',  'broken' => '0', 'photo' => 'foto2.jpg',  'created_by' => '1', 'updated_by' => '2'],
-            ['ruangan_id' => '5',  'name' => 'Spidol',       'total' => '3',  'broken' => '1', 'photo' => 'foto3.jpg',  'created_by' => '1', 'updated_by' => '1'],
-            ['ruangan_id' => '2',  'name' => 'Proyektor',    'total' => '1',  'broken' => '0', 'photo' => 'foto4.jpg',  'created_by' => '1', 'updated_by' => '1'],
-            ['ruangan_id' => '4',  'name' => 'LCD',          'total' => '2',  'broken' => '0', 'photo' => 'foto5.jpg',  'created_by' => '1', 'updated_by' => '2']
+            ['ruangan_id' => '1',  'name' => 'Meja',         'total' => '25', 'broken' => '3', 'photo' => 'barang-20200421040202.png',  'created_by' => '1', 'updated_by' => '1'],
+            ['ruangan_id' => '3',  'name' => 'Papan_Tulis',  'total' => '2',  'broken' => '0', 'photo' => 'barang-20200421041005.jpg',  'created_by' => '1', 'updated_by' => '2'],
+            ['ruangan_id' => '5',  'name' => 'Spidol',       'total' => '3',  'broken' => '1', 'photo' => 'barang-20200421040202.png',  'created_by' => '1', 'updated_by' => '1'],
+            ['ruangan_id' => '2',  'name' => 'Proyektor',    'total' => '1',  'broken' => '0', 'photo' => 'barang-20200421041005.jpg',  'created_by' => '1', 'updated_by' => '1'],
+            ['ruangan_id' => '4',  'name' => 'LCD',          'total' => '2',  'broken' => '0', 'photo' => 'barang-20200421041005.jpg',  'created_by' => '1', 'updated_by' => '2']
         ];
 
         foreach ($listBarang as $barang) {
