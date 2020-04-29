2aaee48dd99f27ef17974b6a7872a74c62a69f3a
ramaarief <ramaarief45@gmail.com>
2020-04-29 20:16:27 +0700

    Retouch BarangExport

--- app/Exports/BarangExport.php
+++ app/Exports/BarangExport.php
@@ -4,14 +4,30 @@ namespace App\Exports;
 
 use App\Barang;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 
-class BarangExport implements FromCollection
+class BarangExport implements FromCollection, WithHeadings, ShouldAutoSize
 {
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-        return Barang::all();
+        return Barang::select('id', 'name', 'total', 'broken', 'created_by', 'updated_by', 'created_at', 'updated_at')->get();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'ID',
+            'Nama Barang',
+            'Total',
+            'Rusak',
+            'Created_by',
+            'Updated_by',
+            'Created At',
+            'Updated At'
+        ];
     }
 }
