0fe789898ff1f3deb24debe17e3739c37b77d22a
ramaarief <ramaarief45@gmail.com>
2020-05-15 01:07:02 +0700

    Update seeder

--- database/seeds/BarangSeeder.php
+++ database/seeds/BarangSeeder.php
@@ -13,11 +13,35 @@ class BarangSeeder extends Seeder
     public function run()
     {
         $listBarang = [
-            ['ruangan_id' => '1',  'name' => 'Meja',         'total' => '25', 'broken' => '3', 'photo' => 'barang-20200421040202.png',  'created_by' => '1', 'updated_by' => '1'],
-            ['ruangan_id' => '3',  'name' => 'Papan_Tulis',  'total' => '2',  'broken' => '0', 'photo' => 'barang-20200421041005.jpg',  'created_by' => '1', 'updated_by' => '2'],
-            ['ruangan_id' => '5',  'name' => 'Spidol',       'total' => '3',  'broken' => '1', 'photo' => 'barang-20200421040202.png',  'created_by' => '1', 'updated_by' => '1'],
-            ['ruangan_id' => '2',  'name' => 'Proyektor',    'total' => '1',  'broken' => '0', 'photo' => 'barang-20200421041005.jpg',  'created_by' => '1', 'updated_by' => '1'],
-            ['ruangan_id' => '4',  'name' => 'LCD',          'total' => '2',  'broken' => '0', 'photo' => 'barang-20200421041005.jpg',  'created_by' => '1', 'updated_by' => '2']
+            ['ruangan_id' => '1',  'name' => 'Meja',         'total' => '25', 'broken' => '3', 'photo' => 'meja.png',  'created_by' => '1', 'updated_by' => '1'],
+            ['ruangan_id' => '1',  'name' => 'Papan_Tulis',  'total' => '2',  'broken' => '0', 'photo' => 'papan_tulis.png',  'created_by' => '1', 'updated_by' => '2'],
+            ['ruangan_id' => '1',  'name' => 'Spidol',       'total' => '5',  'broken' => '1', 'photo' => 'spidol.png',  'created_by' => '1', 'updated_by' => '1'],
+            ['ruangan_id' => '1',  'name' => 'Proyektor',    'total' => '1',  'broken' => '0', 'photo' => 'proyektor.png',  'created_by' => '1', 'updated_by' => '1'],
+            ['ruangan_id' => '1',  'name' => 'LCD',          'total' => '1',  'broken' => '1', 'photo' => 'lcd.png',  'created_by' => '1', 'updated_by' => '2'],
+
+            ['ruangan_id' => '5',  'name' => 'Meja',         'total' => '20', 'broken' => '2', 'photo' => 'meja.png',  'created_by' => '1', 'updated_by' => '1'],
+            ['ruangan_id' => '5',  'name' => 'Papan_Tulis',  'total' => '1',  'broken' => '0', 'photo' => 'papan_tulis.png',  'created_by' => '1', 'updated_by' => '2'],
+            ['ruangan_id' => '5',  'name' => 'Spidol',       'total' => '3',  'broken' => '2', 'photo' => 'spidol.png',  'created_by' => '1', 'updated_by' => '1'],
+            ['ruangan_id' => '5',  'name' => 'Proyektor',    'total' => '1',  'broken' => '1', 'photo' => 'proyektor.png',  'created_by' => '1', 'updated_by' => '1'],
+            ['ruangan_id' => '5',  'name' => 'LCD',          'total' => '1',  'broken' => '0', 'photo' => 'lcd.png',  'created_by' => '1', 'updated_by' => '2'],
+
+            ['ruangan_id' => '7',  'name' => 'Meja',         'total' => '30', 'broken' => '5', 'photo' => 'meja.png',  'created_by' => '1', 'updated_by' => '1'],
+            ['ruangan_id' => '7',  'name' => 'Papan_Tulis',  'total' => '2',  'broken' => '1', 'photo' => 'papan_tulis.png',  'created_by' => '1', 'updated_by' => '2'],
+            ['ruangan_id' => '7',  'name' => 'Spidol',       'total' => '4',  'broken' => '0', 'photo' => 'spidol.png',  'created_by' => '1', 'updated_by' => '1'],
+            ['ruangan_id' => '7',  'name' => 'Proyektor',    'total' => '2',  'broken' => '0', 'photo' => 'proyektor.png',  'created_by' => '1', 'updated_by' => '1'],
+            ['ruangan_id' => '7',  'name' => 'LCD',          'total' => '2',  'broken' => '0', 'photo' => 'lcd.png',  'created_by' => '1', 'updated_by' => '2'],
+
+            ['ruangan_id' => '10',  'name' => 'Meja',         'total' => '15', 'broken' => '1', 'photo' => 'meja.png',  'created_by' => '1', 'updated_by' => '1'],
+            ['ruangan_id' => '10',  'name' => 'Papan_Tulis',  'total' => '2',  'broken' => '1', 'photo' => 'papan_tulis.png',  'created_by' => '1', 'updated_by' => '2'],
+            ['ruangan_id' => '10',  'name' => 'Spidol',       'total' => '1',  'broken' => '0', 'photo' => 'spidol.png',  'created_by' => '1', 'updated_by' => '1'],
+            ['ruangan_id' => '10',  'name' => 'Proyektor',    'total' => '1',  'broken' => '0', 'photo' => 'proyektor.png',  'created_by' => '1', 'updated_by' => '1'],
+            ['ruangan_id' => '10',  'name' => 'LCD',          'total' => '1',  'broken' => '0', 'photo' => 'lcd.png',  'created_by' => '1', 'updated_by' => '2'],
+
+            ['ruangan_id' => '15',  'name' => 'Meja',         'total' => '10', 'broken' => '0', 'photo' => 'meja.png',  'created_by' => '1', 'updated_by' => '1'],
+            ['ruangan_id' => '15',  'name' => 'Papan_Tulis',  'total' => '1',  'broken' => '0', 'photo' => 'papan_tulis.png',  'created_by' => '1', 'updated_by' => '2'],
+            ['ruangan_id' => '15',  'name' => 'Spidol',       'total' => '3',  'broken' => '1', 'photo' => 'spidol.png',  'created_by' => '1', 'updated_by' => '1'],
+            ['ruangan_id' => '15',  'name' => 'Proyektor',    'total' => '2',  'broken' => '1', 'photo' => 'proyektor.png',  'created_by' => '1', 'updated_by' => '1'],
+            ['ruangan_id' => '15',  'name' => 'LCD',          'total' => '2',  'broken' => '0', 'photo' => 'lcd.png',  'created_by' => '1', 'updated_by' => '2']
         ];
 
         foreach ($listBarang as $barang) {
--- database/seeds/RuanganSeeder.php
+++ database/seeds/RuanganSeeder.php
@@ -14,15 +14,31 @@ class RuanganSeeder extends Seeder
     {
         $listRuangan = [
             ['jurusan_id' => '1',   'name' => 'TI-001'],
+            ['jurusan_id' => '1',   'name' => 'TI-002'],
+            ['jurusan_id' => '1',   'name' => 'TI-003'],
             ['jurusan_id' => '1',   'name' => 'TI-004'],
 
-            ['jurusan_id' => '4',   'name' => 'HK-238'],
+            ['jurusan_id' => '4',   'name' => 'HK-231'],
+            ['jurusan_id' => '4',   'name' => 'HK-232'],
 
-            ['jurusan_id' => '10',  'name' => 'PB-012'],
+            ['jurusan_id' => '5',   'name' => 'PB-012'],
+            ['jurusan_id' => '5',   'name' => 'PB-013'],
+            ['jurusan_id' => '5',   'name' => 'PB-014'],
 
-            ['jurusan_id' => '13',  'name' => 'SP-055'],
+            ['jurusan_id' => '8',  'name' => 'SP-045'],
+            ['jurusan_id' => '8',  'name' => 'SP-035'],
+            ['jurusan_id' => '8',  'name' => 'SP-025'],
+            ['jurusan_id' => '8',  'name' => 'SP-015'],
+            ['jurusan_id' => '8',  'name' => 'SP-005'],
 
-            ['jurusan_id' => '15',  'name' => 'KM-023']
+            ['jurusan_id' => '11',  'name' => 'DK-023'],
+            ['jurusan_id' => '11',  'name' => 'DK-024'],
+            ['jurusan_id' => '11',  'name' => 'DK-025'],
+
+            ['jurusan_id' => '13',  'name' => 'HI-112'],
+            ['jurusan_id' => '13',  'name' => 'HI-113'],
+            ['jurusan_id' => '13',  'name' => 'HI-114'],
+            ['jurusan_id' => '13',  'name' => 'HI-115']
         ];
 
         foreach ($listRuangan as $ruangan) {
